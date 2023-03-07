765c4953a4b656ff54bc22adfb587cd64439640c
FavoriitePrime <manhunt055@gmail.com>
2023-03-07 11:03:39 +0300

    Add Filter

--- src/Components/Games/Games.jsx
+++ src/Components/Games/Games.jsx
@@ -1,80 +1,69 @@
 import React from "react";
-import NightLightImg from "../../Images/Games/NightLight.jpg"
-import { Link } from 'react-scroll'
-import "./Games.css"
+import Post from "./Post/Post";
+import NightLightImg from "../../Images/Games/NightLight.jpg";
+import "./Games.css";
+import { useRef, useState } from "react";
 
-export default function Games(){
-    return(
-        <div id="Games" className="games-box">
-        <div className="games-stickyList-box">
-            <div className="games-stickyList">
-                <div className="games-stickyList-title">
-                    Games made with Unity
-                </div>
-                <ul>
-                    <li>
-                        <Link to="Zenith" smooth={true}>
-                            Zenith:The Last City
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="V Rising" smooth={true}>
-                            V Rising
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="Outer Wilds" smooth={true}>
-                            Outer Wilds
-                        </Link>
-                    </li>
-                </ul>
+export default function Games() {
+  const posts = [
+    <Post content="Fortnite" header="Header" image={NightLightImg}></Post>,
+    <Post content="CSGO" header="Header" image={NightLightImg}></Post>,
+    <Post content="Team Fortress" header="Header" image={NightLightImg}></Post>,
+    <Post content="Minecraft" header="Header" image={NightLightImg}></Post>,
+    <Post content="Minesweper" header="Header" image={NightLightImg}></Post>,
+    <Post content="Atomic Heart" header="Header" image={NightLightImg}></Post>,
+    <Post content="Over Cocked" header="Header" image={NightLightImg}></Post>,
+    <Post content="Tomb Rider" header="Header" image={NightLightImg}></Post>,
+    <Post
+      content="Genshin Impact"
+      header="Header"
+      image={NightLightImg}
+    ></Post>,
+    <Post content="Call of Duty" header="Header" image={NightLightImg}></Post>,
+  ];
+  const [filter, setfilter] = useState(posts);
+  //Filter CheckBoxs
+  const inputRef = useRef();
+  const [arcade, setArcade] = useState(false);
+  const [FPS, setFPS] = useState(false);
+  const [ThirdPerson, setThirdPerson] = useState(false);
+  let message;
+  const handleChange = (event) => {
+    console.log(inputRef.current.value);
+    setfilter(
+      posts.filter((post) => {
+        message = post.props.content.toLowerCase();
+        return message.includes(inputRef.current.value.toLowerCase());
+      })
+    );
+    console.log(filter);
+  };
+
+  return (
+    <div id="Games" className="games-box">
+      <div className="games-stickyList-box">
+        <div className="games-stickyList">
+          <div className="games-stickyList-title">Games made with Unity</div>
+          <input
+            ref={inputRef}
+            type="text"
+            id="message"
+            name="message"
+            onInput={handleChange}
+          ></input>
+          <div className="games-stickyList-checkBox-box">
+            <div>
+              <input
+                type="checkbox"
+                checked={arcade}
+                onChange={() => setArcade(!arcade)}
+              />
+              Аркада?
             </div>
-            <div className="games-contentBox">
-                <div id="Zenith" className="games-contentBox-card">
-                    <img className="games-contentBox-card-image" src={NightLightImg} alt="Help" />
-                    <div className="games-contentBox-card-title">
-                        Zenith: The Last City
-                    </div>
-                    <div>
-                        Студия Ramen VR поставила перед
-                        собой неординарную цель: создать
-                        полноценную MMO в VR. Узнайте, какие
-                        инструменты помогли этой инди-студии
-                        превратить Zenith: The Last City в реальность.
-                    </div>
-                </div>
-                <div id="V Rising" className="games-contentBox-card">
-                    <img className="games-contentBox-card-image" src={NightLightImg} alt="Help" />
-                    <div className="games-contentBox-card-title">
-                        V Rising
-                    </div>
-                    <div>
-                        Созданная студией Stunlock Studios
-                        многопользовательская игра на выживание
-                        в мире вампиров стала хитом: за первый
-                        месяц после выпуска ее установили более
-                        чем 2 миллиона игроков. Узнайте, как
-                        технология DOTS помогла воплотить в жизнь
-                        эпический и динамичный открытый игровой мир.
-                    </div>
-                </div>
-                <div id="Outer Wilds" className="games-contentBox-card">
-                    <img className="games-contentBox-card-image" src={NightLightImg} alt="Help" />
-                    <div className="games-contentBox-card-title">
-                        Outer Wilds
-                    </div>
-                    <div>
-                        Приключенческая игра-головоломка от Mobius
-                        Digital получила признание за зацикленный
-                        сюжет и красивые космические визуальные
-                        эффекты. В этом видео арт-директор студии
-                        подробно рассматривает шейдеры океана в
-                        Giant's Deep.
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
-
+        <div className="games-contentBox">{filter}</div>
+      </div>
     </div>
-    )
+  );
 }
---
+++ src/Components/Games/Post/Post.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Post(props) {
+  return (
+    <div>
+      <div className="games-contentBox-card">
+        <img
+          className="games-contentBox-card-image"
+          src={props.image}
+          alt="Help"
+        />
+        <div className="games-contentBox-card-title">{props.header}</div>
+        <div>{props.content}</div>
+      </div>
+    </div>
+  );
+}
